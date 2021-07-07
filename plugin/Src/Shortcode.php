7473e315b0ce7941bb9caba8cb010f0b5d226baf
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-07-07 02:33:26 +0000

    refactor short code methods and params

--- plugin/Src/Shortcode.php
+++ plugin/Src/Shortcode.php
@@ -22,8 +22,8 @@ class Shortcode {
 		public function __construct() {
 
         	$this->template = new Templating();
-			add_shortcode( PLUGIN_TEXT_DOMAIN . '_page' , array( $this, 'display_page' ) );
-        	add_shortcode( PLUGIN_TEXT_DOMAIN . '_button', [$this, 'create_payment_buton'] );
+			add_shortcode( WP_FAC_HOSTED_PAGE_TEXT_DOMAIN . '_page' , array( $this, 'display_page' ) );
+        	add_shortcode( WP_FAC_HOSTED_PAGE_TEXT_DOMAIN . '_button', [$this, 'create_payment_buton'] );
 		}
 
 	   /**
@@ -36,36 +36,52 @@ class Shortcode {
 		public function display_page($atts) {
 
 			$data = shortcode_atts( array(
-										'page' => 'payment_page',
+										'page' => 'page.success',
             							'transaction_id' => get_query_var('transaction_id', null)
 									), $atts );
-        	$template = "part-{$data['page']}";
+        	$template = "{$data['page']}";
 			if(isset($_GET['ID']) && isset($_GET['RespCode']) && isset($_GET['ReasonCode'])) {
-            	$result = wp_fac_hosted_page_get_result($_GET['ID']);
-            	if(is_wp_error($result)) {
-                	$template = "part-page.error";
-                    $data = ['code' => $result->get_error_code(), 'message' => $result->get_error_message(), 'transaction_id' => $data['transaction_id'] ];
-                } else { 
-                	$template = "part-page.success";
-                	$data = $result;
-               }
+            	$data = wp_fac_hosted_page_get_result($_GET['ID']);
+            	if(is_wp_error($data)) {
+                	$template = "page.error";
+                    $data = ['code' => $data->get_error_code(), 'message' => $data->get_error_message(), 'transaction_id' => $data['transaction_id'] ];
+                }
             }
         	
-			return $this->template->render($template, $data, true );
+			ob_start();
+			$this->template->get_template_part("part",$template, $data );
+        	$file_content = ob_get_contents();
+			ob_end_clean ();
+        
+        	return $file_content;
 		}
-
+		
 		public function create_payment_buton($atts)
         {
         	$data = shortcode_atts( array(
-										'method' => 'hosted_page',
-            							'text' => 'Pay Now',
-            							'nounce' => wp_create_nonce( '_fac_nounce' ),
-            							'redirect_url' => '',
             							'amount' => '',
             							'currency' => 'USD',
             							'transaction_id' => '',
+            							'text' => 'Pay Now'
 									), $atts );
+        	
+        	$template = 'payment.button';
+    		$payment_link = fac_wp_hosted_page_get_payment_link($data);
+    		if(is_wp_error($payment_link )) {
+        		$template = 'payment.button.error';
+        		$data = ['code' => $payment_link->get_error_code(), 'message' => $payment_link->get_error_message() ];
+        	} else {
+        		$data = [
+        			'text' => $data['text'],
+        			'fac_hosted_page_url' => $payment_link['data']['url']
+                ];
+        	}
+        	
+        	ob_start();
+			$this->template->get_template_part("part",$template, $data );
+        	$file_content = ob_get_contents();
+			ob_end_clean ();
         
-			return $this->template->render('part-payment.button', $data );
+        	return $file_content;
         }
 	}
