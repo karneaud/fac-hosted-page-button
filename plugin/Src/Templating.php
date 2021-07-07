1bfb665b45714b92bf38864a738ff679fdf66880
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-07-07 02:34:18 +0000

    refactor methods to resemble wordpress template function

--- plugin/Src/Templating.php
+++ plugin/Src/Templating.php
@@ -8,29 +8,22 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 class Templating
 {
-    /**
-     * Render a template.
-     *
-     * @param string $path Path to the template to render, relative to the
-     *                     `templates/` directory
-     * @param array  $data This array can be referenced in the template
-     * @param bool	$return Return the template markup. False to print.
-     */
-    public function render($path, $data = array(), $return = false)
-    {
-    	$file = __DIR__."/../../asset/templates/$path.php";
-    	if($return) 
-        {
-        	ob_start();		
-			include($file);
-			$contents = ob_get_contents();
-			ob_end_clean();
-        
-        	return <<<TEMPLATE
-            {$contents}
-            TEMPLATE;
-        }
     
-        include realpath($file);
+	/**
+	 * Adaptation of wordpress get_template_part
+	 * @param string $slug template file slug
+	 * @param string $name template fila name
+	 * @param array $args arguments to pass to template file
+	 * @return void
+	 */ 
+	public function get_template_part(string $slug, string $name = null, $args = []) 
+    {
+    	if(get_template_part($slug, $name, $args )) 
+        else if(get_template_part(WP_FAC_HOSTED_PAGE_TEXT_DOMAIN . "/{$slug}", $name, $args )) 
+        
+        load_template( 	sprintf("%s/%s.php", plugin_dir_path(__FILE__) , (empty($name)? $slug : "{$slug}-{$name}") ) ,
+                      	array_key_exists('require_once', $args ), 
+                      	$args 
+                     );
     }
 }
