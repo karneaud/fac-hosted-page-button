79f59a36e9480bd91c405375b87088421c09d334
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-07-07 05:59:41 +0000

    update templates to use extracted vars

--- templates/part-page.error.php
+++ templates/part-page.error.php
@@ -1,3 +1,3 @@
-<?php do_action('wp-fac-hosted-page_before_page_error', $data['transaction_id']);  ?>
-	<p class="wp_fac_hosted_page page_error">Error: <?php print $data['code'] ?> - <?php print $data['message'] ?></p>
-<?php do_action('wp-fac-hosted-page_after_page_error', $data['transaction_id']); ?>
+<?php do_action('wp-fac-hosted-page_before_page_error', $transaction_id);  ?>
+	<p class="wp_fac_hosted_page page_error">Error: <?php print $code ?> - <?php print $message ?></p>
+<?php do_action('wp-fac-hosted-page_after_page_error', $transaction_id); ?>
