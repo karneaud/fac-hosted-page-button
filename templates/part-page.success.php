5730f68f95dc149b3d476e26241b7a9d6874995b
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-07-10 00:35:43 +0000

    update

--- templates/part-page.success.php
+++ templates/part-page.success.php
@@ -1,5 +1,5 @@
-<?php //do_action(WP_FAC_HOSTED_PAGE_TEXT_DOMAIN . '_before_page_success', $transaction_id); ?>
+<?php do_action(WP_FAC_HOSTED_PAGE_TEXT_DOMAIN . '_before_page_success', $transaction_id); ?>
 	<h3>Payment Processed Successfully</h3>
 	<p>Transaction Reference #<?php print $transaction_id ?></p>
 	<p class="wp_fac_hosted_page page_success_messasge"><?php print $message; ?></p>
-<?php //do_action(WP_FAC_HOSTED_PAGE_TEXT_DOMAIN . '_after_page_success', $transaction_id ); ?>
+<?php do_action(WP_FAC_HOSTED_PAGE_TEXT_DOMAIN . '_after_page_success', $transaction_id ); ?>
